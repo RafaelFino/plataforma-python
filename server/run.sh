aad00df6d3ab635fcae9007c8eb3b4f0e1257596
Rafael Gottardi <rafael.gottardi@c6bank.com>
2021-08-11 10:49:08 -0300

    fix compose

--- server/run.sh
+++ server/run.sh
@@ -2,6 +2,16 @@
 
 echo "Starting server..."
 
+export LC_ALL=C.UTF-8
+
+export LANG=C.UTF-8
+
+cd /opt/server
+
+echo "Running pip install..."
+pip install --no-cache-dir -r requirements.txt
+
+echo "calling uvicorn..."
 uvicorn app.main:app --reload --port 8080 --host 0.0.0.0 --log-level trace
 
 echo "Server stopped"
