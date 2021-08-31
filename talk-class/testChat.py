68113f9b50d96cf3d24f5026677a926bf9a91f34
Rafael Gottardi <rafael.gottardi@c6bank.com>
2021-08-31 16:21:14 -0300

    improve TALKER

---
+++ talk-class/testChat.py
@@ -0,0 +1,9 @@
+from talker import chat
+
+chat = chat.Chat("Fino", "Aluno")
+
+chat.Speak("""Olá você, tudo bem?""")
+chat.StudentComment("""Tudo legal sim, e agora?""")
+chat.Speak("""Bom, isso aqui é só um teste para o chat...""")
+chat.Question("""Mas será que vão usar?""")
+chat.Speak("""Sei lá... mas vale tentar, não acha?""")
